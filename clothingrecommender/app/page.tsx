032e1ec887a1b74bf27adda42b2b202d923fdaee
koicoco <chenningnuo@gmail.com>
2026-02-07 15:04:19 -0500

    remember to save next time

--- clothingrecommender/app/page.tsx
+++ clothingrecommender/app/page.tsx
@@ -1,16 +1,5 @@
 export default function Home() {
   return (
-<<<<<<< HEAD
-    <div>
-      <h1>
-        Welcome to the Clothing Recommender App!
-      </h1>
-      <a href="/profile" className="border-8 border-green-900 border-dashed fill-cyan-500 drop-shadow-lg drop-shadow-indigo-500"> 
-        Profile Page
-      </a>
-    </div>
-=======
     <p>Home Page</p>
->>>>>>> main
   );
 }
